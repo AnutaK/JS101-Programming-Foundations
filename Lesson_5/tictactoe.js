de57a8b147a378ddea009b4d74e21d6bb8314a07
AnutaK <akvaisman@gmail.com>
2020-06-24 23:34:44 -0700

    Added a borad object

--- Lesson_5/tictactoe.js
+++ Lesson_5/tictactoe.js
@@ -1,13 +1,29 @@
-console.log("");
-console.log("     |     |");
-console.log("     |     |");
-console.log("     |     |");
-console.log("-----+-----+-----");
-console.log("     |     |");
-console.log("     |     |");
-console.log("     |     |");
-console.log("-----+-----+-----");
-console.log("     |     |");
-console.log("     |     |");
-console.log("     |     |");
-console.log("");
+function displayBoard(board) {
+  console.log("");
+  console.log("     |     |");
+  console.log("     |     |");
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log("     |     |");
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log("     |     |");
+  console.log("     |     |");
+  console.log("");
+}
+
+let board = {
+  1: "X", // top left
+  2: " ", // top center
+  3: " ", // top right
+  4: " ", // middle left
+  5: "O", // center
+  6: " ", // middle right
+  7: " ", // bottom left
+  8: " ", // bottom center
+  9: "X", // bottom right
+};
+
+displayBoard(board);
